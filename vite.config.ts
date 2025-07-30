f43b6db54a25ddec8675e3088ef111eebf3b01c2
venkata Krishna Reddy Dondeti <venkatakrishna9642@gmail.com>
2025-07-30 15:09:12 +0530

    Remove unused Electron configuration and dependencies from package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import {fileURLToPath} from 'url';
-import electron from 'vite-plugin-electron';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    electron({
-      entry: 'electron/main.js',
-    }),
+    react()
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
